72fe00d5157e5f3647aab66fbbbca4c464a973ec
mikael <mikael.stl05@gmail.com>
2022-11-09 17:58:43 -0300

    Adicionando resposta para a questão 3 (CBB)

--- CBBAtividadePratica3.java
+++ CBBAtividadePratica3.java
@@ -1,12 +1,12 @@
-public class CBBAtividadePratica1{
+public class CBBAtividadePratica3{
     public static void main(String[] args){
         int[] arrayInt = new int[100];
 
         for (int i = 0; i < 100; i++){
             arrayInt[i] = i;
         }
-
-        for (int j = 0; j < 100; j++){
+        
+        for (int j = 0; j < arrayInt.length; j++){
             System.out.print(arrayInt[j] + " ");
         }
     }
